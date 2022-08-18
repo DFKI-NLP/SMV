8ec66c19566fa66a8f6e37786bf3ef5423e6df70
Maximilian Nasert <maximilian.nasert@gmx.de>
2022-08-18 14:21:21 +0200

    small update

--- demo_json.py
+++ demo_json.py
@@ -5,7 +5,7 @@ import src.search_methods.fastexplain as fe
 
 
 if __name__ == "__main__":
-    config_pathes = ["./configs/quantile_dev.yml"]
+    config_pathes = ["./configs/quantile_dev.yml",]
     jsons = []
     with multiprocessing.Pool(4) as pool:  # assuming you have at least 16 gigs of ram -> assume each process needs 2gigs of ram
         _ = pool.imap(fe.explain_json, config_pathes)
